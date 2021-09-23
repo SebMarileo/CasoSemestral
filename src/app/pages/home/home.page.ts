d4c7aa2d517049843bf23d85782757c67c731bcc
SebMarileo <seba.marileo.j@gmail.com>
2021-09-23 11:43:41 -0300

    confirmo

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,19 @@ export class HomePage {
 
   constructor() {}
 
+  user = new FormGroup({
+    eluser : new FormControl('')
+  })
+
+  lista_user=new Array();
+  usuario:any;
+  grabar(){
+    this.usuario={
+      user: this.user.controls.eluser.value
+    }
+    this.lista_user.push(this.usuario);
+    var datos = this.lista_user;
+    localStorage.setItem('Usuarios',JSON.stringify(datos));
+    alert('grabo')
+  }
 }
